34789cd98dc6a9cd54e7192f6b4adb6621889cd3
Matheus Lima Roichman <roichmankill@gmail.com>
2025-02-01 18:37:56 -0300

    feat: added header

---
+++ src/view/layout/components/header.tsx
@@ -0,0 +1,28 @@
+import { LogoWhite } from "@/assets/illustrations/logo-white"
+import { Link } from "react-router"
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/cost-report", label: "Cost Report" },
+  { href: "/ride-history", label: "Ride History" },
+]
+
+export function Header() {
+  return (
+    <header className="bg-black text-white p-4">
+      <div className="flex justify-between items-center max-w-5xl mx-auto">
+        <LogoWhite className="h-10 w-fit" />
+
+        <nav className="hidden md:block">
+          <ul className="flex items-center gap-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link to={link.href} className="hover:underline">{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/view/layout/index.tsx
+++ src/view/layout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
+import { Header } from "./components/header";
 
 export function RootLayout() {
   return (
     <>
+      <Header />
       <Outlet />
-    </>
+    </ >
   )
 }
