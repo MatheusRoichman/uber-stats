117d624d7d26b7ac6ac3af6cc27a4ad140f00559
Matheus Lima Roichman <roichmankill@gmail.com>
2025-02-01 23:39:47 -0300

    refactor: loop only through rides without begin trip address

--- src/app/services/rides/list-rides.ts
+++ src/app/services/rides/list-rides.ts
@@ -1,48 +1,52 @@
-import type { QueryClient } from "@tanstack/react-query";
+import type { QueryClient } from '@tanstack/react-query';
 
-import { parse } from "papaparse";
+import { parse } from 'papaparse';
 
-import type { Ride } from "@/app/entities/ride";
+import type { Ride } from '@/app/entities/ride';
 
-import { getAddressFromCoords } from "../address/get-address-from-coords";
+import { getAddressFromCoords } from '../address/get-address-from-coords';
 
 interface ListRidesParams {
-	csvContent: string;
-	queryClient: QueryClient;
+  csvContent: string;
+  queryClient: QueryClient;
 }
 
 export async function listRides({ csvContent, queryClient }: ListRidesParams) {
-	const parseResult = parse<Ride>(csvContent, {
-		header: true,
-		dynamicTyping: true,
-		skipEmptyLines: true,
-	});
+  try {
+    const parseResult = parse<Ride>(csvContent, {
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+    });
 
-	if (!parseResult.data.some((ride) => !ride.begintrip_address)) {
-		return parseResult.data;
-	}
+    if (!parseResult.data.some((ride) => !ride.begintrip_address)) {
+      return parseResult.data;
+    }
 
-	const addresses = await Promise.all(
-		parseResult.data
-			.filter((ride) => !ride.begintrip_address)
-			.map((ride) => {
-				return queryClient.fetchQuery({
-					queryKey: ["address", ride.begintrip_lat, ride.begintrip_lng],
-					queryFn: () =>
-						getAddressFromCoords({
-							lat: ride.begintrip_lat,
-							lng: ride.begintrip_lng,
-						}),
-					staleTime: 1000 * 60 * 60 * 24 * 14, // 2 weeks
-				});
-			}),
-	);
+    const ridesWithoutBeginTripAddress = parseResult.data.filter(
+      (ride) => !ride.begintrip_address,
+    );
 
-	for (let i = 0; i < parseResult.data.length; i++) {
-		if (!parseResult.data[i].begintrip_address) {
-			parseResult.data[i].begintrip_address = addresses[i];
-		}
-	}
+    const addresses = await Promise.all(
+      ridesWithoutBeginTripAddress.map((ride) => {
+        return queryClient.fetchQuery({
+          queryKey: ['address', ride.begintrip_lat, ride.begintrip_lng],
+          queryFn: () =>
+            getAddressFromCoords({
+              lat: ride.begintrip_lat,
+              lng: ride.begintrip_lng,
+            }),
+          staleTime: 1000 * 60 * 60 * 24 * 14, // 2 weeks
+        });
+      }),
+    );
 
-	return parseResult.data;
+    for (let i = 0; i < ridesWithoutBeginTripAddress.length; i++) {
+      ridesWithoutBeginTripAddress[i].begintrip_address = addresses[i];
+    }
+
+    return parseResult.data;
+  } catch {
+    throw new Error("Failed to read the rides from the CSV file. Please try again.");
+  }
 }
