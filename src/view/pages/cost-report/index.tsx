37b5d80820e3ce957e3939cfac23e84cbc4c85fb
Matheus Lima Roichman <roichmankill@gmail.com>
2025-02-01 23:06:11 -0300

    feat: added cost report page base layout

--- src/view/pages/cost-report/index.tsx
+++ src/view/pages/cost-report/index.tsx
@@ -1,7 +1,9 @@
 export function CostReport() {
   return (
-    <>
-      <h1>Cost Report</h1>
-    </>
+    <section className="p-8">
+      <div className="max-w-5xl mx-auto">
+        <h1>Cost Report</h1>
+      </div>
+    </section>
   )
 }
